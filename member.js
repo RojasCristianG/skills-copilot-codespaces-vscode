1c5295f25d27484d9bdea62fc212129fa5a6dfb4
Cristian Rojas <80907155+RojasCristianG@users.noreply.github.com>
2023-11-21 18:09:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,28 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberName = document.getElementById("memberName");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsList = document.getElementById("memberSkillsList");
+    var memberSkillsListItems = document.getElementsByClassName("memberSkillsListItem");
+    var memberSkillsListItem = document.getElementsByClassName("memberSkillsListItem");
+    var memberSkillsListItemText = document.getElementsByClassName("memberSkillsListItemText");
+    var memberSkillsListItemTextSpan = document.getElementsByClassName("memberSkillsListItemTextSpan");
+    var memberSkillsListItemTextSpanIcon = document.getElementsByClassName("memberSkillsListItemTextSpanIcon");
+    var memberSkillsListItemTextSpanIconI = document.getElementsByClassName("memberSkillsListItemTextSpanIconI");
+    var memberSkillsListItemTextSpanIconIi = document.getElementsByClassName("memberSkillsListItemTextSpanIconIi");
+    var memberSkillsListItemTextSpanIconIii = document.getElementsByClassName("memberSkillsListItemTextSpanIconIii");
+    var memberSkillsListItemTextSpanIconIv = document.getElementsByClassName("memberSkillsListItemTextSpanIconIv");
+    var memberSkillsListItemTextSpanIconV = document.getElementsByClassName("memberSkillsListItemTextSpanIconV");
+    var memberSkillsListItemTextSpanIconVi = document.getElementsByClassName("memberSkillsListItemTextSpanIconVi");
+    var memberSkillsListItemTextSpanIconVii = document.getElementsByClassName("memberSkillsListItemTextSpanIconVii");
+    var memberSkillsListItemTextSpanIconViii = document.getElementsByClassName("memberSkillsListItemTextSpanIconViii");
+    var memberSkillsListItemTextSpanIconIx = document.getElementsByClassName("memberSkillsListItemTextSpanIconIx");
+    var memberSkillsListItemTextSpanIconX = document.getElementsByClassName("memberSkillsListItemTextSpanIconX");
+    var memberSkillsListItemTextSpanIconXi = document.getElementsByClassName("memberSkillsListItemTextSpanIconXi");
+    var memberSkillsListItemTextSpanIconXii = document.getElementsByClassName("memberSkillsListItemTextSpanIconXii");
+    var memberSkillsListItemTextSpanIconXiii = document.getElementsByClassName("memberSkillsListItemTextSpanIconXiii");
+    var memberSkillsListItemTextSpanIconXiv = document.getElementsByClassName("memberSkillsListItemTextSpanIconXiv");
+    var memberSkillsListItemTextSpanIconXv = document.getElementsByClassName("memberSkillsListItemTextSpanIconXv");
+    var memberSkillsListItemTextSpanIconXvi = document.getElementsByClassName("memberSkillsListItemTextSpanIconXvi");
+    var memberSkillsListItemTextSpanIconXvii
